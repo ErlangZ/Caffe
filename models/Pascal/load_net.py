8266898f83dfc6afd5e65c094d8876bd510ad65a
zhengwenchao <zhengwenchao@baidu.com>
2017-03-01 13:34:44 +0800

    FIX: test in net

--- models/Pascal/load_net.py
+++ models/Pascal/load_net.py
@@ -13,32 +13,45 @@ Date: 2017/03/01 10:36:35
 """
 import sys
 sys.path.append('/home/erlangz/Caffe/build/install/python/')
-net_def_prototxt = '/home/erlangz/Caffe/models/Pascal/yolo_tiny_deploy.prototxt'
-trained_net_caffemodel = '/home/erlangz/Caffe/models/Pascal/caffe_yolo_train_iter_50000.caffemodel'
+net_def_prototxt = '/home/erlangz/Caffe/models/Pascal/yolo_deploy.prototxt'
+trained_net_caffemodel = '/home/erlangz/Caffe/models/Pascal/caffe_yolo_train_iter_10000.caffemodel'
+images_dir = '/home/erlangz/darknet/Data/VOCdevkit/VOCdevkit/VOC2012/JPEGImages/'
 
+batchsize = 1
 import caffe
 import numpy as npy
 import os
 
+import cv2
+#mat = cv2.imread(os.path.join(images_dir, '2008_007788.jpg'))
+#images = [ mat, ]
+images = [os.path.join(images_dir, '2008_007788.jpg'),]
+
 # setup net with ( structure definition file ) + ( caffemodel ), in test mode
 net = caffe.Net(net_def_prototxt, trained_net_caffemodel, caffe.TEST)
 # add preprocessing
-#transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-#transformer.set_transpose('data', (2,0,1)) # height*width*channel -> channel*height*width
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_transpose('data', (2,0,1)) # height*width*channel -> channel*height*width
 #mean_file = np.array([104,117,123]) 
 #transformer.set_mean('data', mean_file) #### subtract mean ####
 #transformer.set_raw_scale('data', 255) # pixel value range
-#transformer.set_channel_swap('data', (2,1,0)) # RGB -> BGR
+transformer.set_channel_swap('data', (2,1,0)) # RGB -> BGR
 
 # set test batchsize
-#data_blob_shape = net.blobs['data'].data.shape
-#data_blob_shape = list(data_blob_shape)
-#net.blobs['data'].reshape(batchsize, data_blob_shape[1], data_blob_shape[2], data_blob_shape[3])
+data_blob_shape = net.blobs['data'].data.shape
+data_blob_shape = list(data_blob_shape)
+net.blobs['data'].reshape(batchsize, data_blob_shape[1], data_blob_shape[2], data_blob_shape[3])
 
 # load data, len(images) = batchsize
-#net.blobs['data'].data[...] = map(lambda x: transformer.preprocess('data',caffe.io.load_image(x)), images)
+net.blobs['data'].data[...] = map(lambda x: transformer.preprocess('data',caffe.io.load_image(x)), images)
 
+import math
+def sigmoid(x):
+    return 1.0 / (1.0 + math.exp(-x))
 # process the data through network
-#out = net.forward()
-#predict = np.argmax(out['prob'], axis=1)
+out = net.forward()
+print out['labels']
+for k in out:
+    if 'ip2-' in k:
+        print k, sigmoid(float(out[k]))
 
